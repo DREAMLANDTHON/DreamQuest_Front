d15a1ccf8753232b3a57d645526a1d0f01a85789
mymo <106878420+mymooss@users.noreply.github.com>
2023-07-29 05:02:02 +0900

    [TEST]

--- src/pages/SelectDreamQuest.js
+++ src/pages/SelectDreamQuest.js
@@ -1,10 +1,33 @@
 import React from "react";
 
+// Custom Box component
+const Box = ({ children }) => {
+  return (
+    <div
+      style={{
+        border: "1px solid black",
+        padding: "10px",
+        borderRadius: "5px",
+        background: "#f0f0f0",
+        width: "fit-content",
+      }}
+    >
+      {children}
+    </div>
+  );
+};
+
 const SelectDreamQuest = () => {
-    return(
-        <>
-        셀렉 ~
-        </>
-    )
-}
-export default SelectDreamQuest
+  return (
+    <>
+      <p>나의 드림퀘스트가 완성되었어요!</p>
+      <br />
+      <p>마음에 드는 드림 퀘스트를 골라주세요.</p>
+
+      {/* Using the custom Box component */}
+      <Box>고구마 먹고 춤추기</Box>
+    </>
+  );
+};
+
+export default SelectDreamQuest;
