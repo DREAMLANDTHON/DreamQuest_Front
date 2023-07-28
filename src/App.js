ca15e4c7878be9f13ceaee7336ff156ae95f1827
jung0115 <jungim7490@gmail.com>
2023-07-28 10:02:48 +0900

    [CHORE] 필요없는 파일 삭제, 폴더 생성, Router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes, // Switch (v5) -> Routes (v6)
+  Route
+} from "react-router-dom";
+import Home from "./routes/Home"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Router basename={process.env.PUBLIC_URL}>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  </Router>;
 }
 
 export default App;
