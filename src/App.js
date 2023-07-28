2cec766daf7a05b639761dfb5390f421b1870a7e
jung0115 <jungim7490@gmail.com>
2023-07-28 10:18:46 +0900

    [ADD] 나의 버킷리스트 페이지 생성

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import {
   Routes, // Switch (v5) -> Routes (v6)
   Route
 } from "react-router-dom";
-import Home from "./routes/Home"
+import MyBucketList from "./pages/MyBucketList";
 
 function App() {
   return <Router basename={process.env.PUBLIC_URL}>
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<MyBucketList />} />
     </Routes>
   </Router>;
 }
---
+++ src/pages/MyBucketList.js
@@ -0,0 +1,18 @@
+// 나의 버킷리스트 페이지
+
+import styled from "styled-components";
+
+const MyBucketList = () => {
+  return (
+    <Container>
+      
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  display: block;
+  position: relative;
+`;
+
+export default MyBucketList;
