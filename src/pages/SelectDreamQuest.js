590de1f58dc36d4c65d0becb96ad7acfab25dc95
jung0115 <jungim7490@gmail.com>
2023-07-29 08:10:16 +0900

    [FEAT] #20 - 생성된 버킷리스트 중 선택

--- src/pages/SelectDreamQuest.js
+++ src/pages/SelectDreamQuest.js
@@ -1,51 +1,21 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+
 import styled from "styled-components";
+import palette from "../styles/colorPalette";
+
 import axios from "axios";
+
+import SelectQuest from "../components/SelectQuest";
 import UnselectQuest from "../components/UnselectQuest"; // UnselectQuest 컴포넌트를 import합니다.
 
-const Button = styled.button`
-  width: 50%;
-  height: 52px;
-  border-radius: 8px;
-  background-color: #8B5C97;
-  font-family: NotoSansKR-Regular;
-  font-size: 14px;
-  color: white;
-  border: none;
-`;
-
-const Button2 = styled.button`
-  width: 50%;
-  height: 52px;
-  border-radius: 8px;
-  background-color: white;
-  font-family: NotoSansKR-Regular;
-  font-size: 14px;
-  color: #8B5C97;
-  border: none;
-  border: 1px solid #8B5C97;
-`;
-
-const Box = ({ children }) => {
-  return (
-    <div
-      style={{
-        border: "1px solid black",
-        padding: "10px",
-        borderRadius: "5px",
-        background: "#f0f0f0",
-        width: "fit-content",
-      }}
-    >
-      {children}
-    </div>
-  );
-};
+const emoji = ["❤️", "🧡", "💛", "💚", "💙"];
 
 const SelectDreamQuest = () => {
   const navigate = useNavigate();
   const [bucketData, setBucketData] = useState([]); // 버킷 데이터를 저장할 상태를 생성합니다.
+
+
   const url = "http://localhost:8080"
   useEffect(() => {
     // 컴포넌트가 마운트될 때 한 번만 실행되는 비동기 함수를 생성합니다.
@@ -54,43 +24,149 @@ const SelectDreamQuest = () => {
       .post(url + '/chat/list')
       .then((response) => {
         // 받아온 데이터를 상태로 저장합니다.
-        setBucketData(response.data[0]);
+        //setBucketData(response.data);
+        let list = response.data;
+        let idx = 0;
+        for(let i = (list.length - 5); i < list.length ; i++) {
+          setBucketData((currentArray) => [...currentArray, [idx++, emoji[i%5], list[i].bucket, list[i].id]]);
+        }
       })
       .catch((error) => {
         // 에러 처리
         console.error("Error fetching bucket data:", error);
       });
+
+      //console.log(bucketData);
+
   }, []);
 
+  useEffect(() => {
+  }, [bucketData]);
+
   const handleSave = () => {
+    for(let i = 0; i < 5; i++) {
+      if(!selected[i]) {
+        deleteBucket(bucketData[i][3]);
+      }
+    }
+
     navigate("/");
   };
+
+  async function deleteBucket(bucketId) {
+    //const response = 
+    await axios.delete(
+      url + `/chat/${bucketId}`
+    );
+  
+    //let result = response.data;
+    //console.log(result);
+  }
 
   const handleRetry = () => {
     window.location.reload();
   };
 
+  const [selected, setSelected] = useState(Array(bucketData.length).fill(false));
+
+  const onSelected = (index) => {
+    // selected 배열의 복사본을 만들어서 직접 상태를 수정하는 것을 피하기 위해 spread 구문을 사용합니다.
+    const updatedSelected = [...selected];
+    updatedSelected[index] = !updatedSelected[index];
+    setSelected(updatedSelected);
+  };
+
+
   return (
-    <>
-      <p>나의 드림퀘스트가 완성되었어요!</p>
-      <br />
-      <p>마음에 드는 드림 퀘스트를 골라주세요.</p>
+    <Container>
+      <Title>나의 드림퀘스트가 완성되었어요!</Title>
+      <Notice>마음에 드는 드림 퀘스트를 골라주세요.</Notice>
 
-      {/* Using the custom Box component */}
-      <Box>고구마 먹고 춤추기</Box>
-      <Button onClick={handleSave}>저장하기</Button>
-      <br />
-      <Button2 onClick={handleRetry}>한번 더 돌리기</Button2>
+      <DreamList>
+        {/* UnselectQuest 컴포넌트에 데이터를 props로 전달합니다. */}
+        {bucketData.map((bucket) => (
+          <div onClick={() => onSelected(bucket[0])}>
+          {selected[bucket[0]] ?
+          (
+            <SelectQuest
+              emoji={bucket[1]}
+              content={bucket[2]}/>
+          ) : (
+            <UnselectQuest
+              emoji={bucket[1]}
+              content={bucket[2]}/>
+          )}
+          </div>
+        ))}
+      </DreamList>
 
-      {/* UnselectQuest 컴포넌트에 데이터를 props로 전달합니다. */}
-      {bucketData.map((bucket) => (
-        <UnselectQuest
-          emoji = {'😊'} // 고유한 key 값을 지정합니다.
-          content={bucket.bucket} // "bucket"만 전달합니다.
-        />
-      ))}
-    </>
+      <CompleteBtn onClick={handleSave}>드림퀘스트 저장하기</CompleteBtn>
+      <ReloadBtn onClick={handleRetry}>한 번 더 돌리기</ReloadBtn>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  display: block;
+  position: relative;
+  height: 100vh;
+  min-width: 100vw;
+  min-height: 100vh;
+  background-color: ${palette.white};
+`;
+
+const Title = styled.div`
+  margin-top: 27.5vw;
+  font-family: 'NotoSansKR-Bold';
+  font-size: 20px;
+  color: ${palette.black};
+  text-align: center;
+`;
+const Notice = styled.div`
+  margin-top: 1vw;
+  font-family: 'NotoSansKR-Regular';
+  font-size: 16px;
+  color: ${palette.black};
+  text-align: center;
+`;
+
+const DreamList = styled.div`
+  margin-top: 12vw;
+  margin-left: 4vw;
+  margin-right: 4vw;
+`;
+
+const CompleteBtn = styled.div`
+  padding-top: 4vw;
+  padding-bottom: 4vw;
+  padding-left: 14vw;
+  padding-right: 14vw;
+  border-radius: 1.8vw;
+  margin-top: 22vw;
+  margin-bottom: 2vw;
+  margin-left: 4vw;
+  margin-right: 4vw;
+  background-color: ${palette.mainColor};
+  font-family: 'NotoSansKR-Medium';
+  font-size: 14px;
+  color: ${palette.white};
+  text-align: center;
+`;
+const ReloadBtn = styled.div`
+  padding-top: 4vw;
+  padding-bottom: 4vw;
+  padding-left: 14vw;
+  padding-right: 14vw;
+  margin-bottom: 3vw;
+  margin-left: 4vw;
+  margin-right: 4vw;
+  border: 1px solid ${palette.mainColor};
+  border-radius: 1.8vw;
+  background-color: ${palette.white};
+  font-family: 'NotoSansKR-Medium';
+  font-size: 14px;
+  color: ${palette.mainColor};
+  text-align: center;
+`;
 
 export default SelectDreamQuest;
