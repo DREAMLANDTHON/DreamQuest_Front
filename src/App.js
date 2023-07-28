8e9338091b78575738f0e98662d164263bfad909
mymo <106878420+mymooss@users.noreply.github.com>
2023-07-28 10:33:15 +0900

    [ADD] question-page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
-
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import Main from './pages/Main';
 const App = () => {
   return(
-    <>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Main />}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 export default App;
---
+++ src/pages/Main.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Question from "./Question";
+
+const Main = () => {
+    return(
+        <>
+        <Question />
+        </>
+    )
+}
+export default Main
