c4cf406efd7a25929e0dbcfe80989609385b9cfb
JeongMi <76805879+jung0115@users.noreply.github.com>
2023-07-29 11:53:30 +0900

    [FEAT] 애니메이션 추가

--- src/pages/MakeDreamList.js
+++ src/pages/MakeDreamList.js
@@ -42,7 +42,7 @@ const MakeDreamListComponent = styled.div`
   height: 100vh;
   position: relative;
   font-family: NotoSansKR-Regular;
-  font-size: 14px;
+  font-size: 15px;
 `;
 
 const StyledParagraph = styled.p`
@@ -62,16 +62,16 @@ const Button = styled.button`
   border: none;
 `;
 
-const MakeDreamList = () => {
+
+const MakeDreamList = () =>{
   const navigate = useNavigate();
 
   const handleStartClick = () => {
     navigate("/question");
   };
-
-  return (
-    <MakeDreamListComponent>
-      <p style={{ fontFamily: "NotoSansKR-Bold", fontSize: "20px" }}>
+    return (
+      <MakeDreamListComponent>
+        <p style={{ fontFamily: "NotoSansKR-Bold", fontSize: "20px" }}>
         드림퀘스트 만들기
       </p>
       <p>내가 살아온 여정을 돌아보며</p>
@@ -79,16 +79,40 @@ const MakeDreamList = () => {
       <p style={{ marginTop: "-20px" }}>
         숨어있던 드림퀘스트를 발견해보세요.
       </p>
-      <MarqueeText>
-        <StyledParagraph>요리학원 다니기</StyledParagraph>
-        <StyledParagraph>바다에서 사진찍기</StyledParagraph>
-        <StyledParagraph>영어로된 에세이 읽기</StyledParagraph>
-        <StyledParagraph>한국사 관련 영화보기</StyledParagraph>
-      </MarqueeText>
-      <Button onClick={handleStartClick}>시작하기</Button>
-      <Link to="/" style={{marginTop:"15px"}}>돌아가기</Link> {/* Link 컴포넌트를 사용하여 링크를 지정합니다. */}
-    </MakeDreamListComponent>
-  );
-};
+        <p>
+          <marquee>시간을 내어 명상하기</marquee>
+          {/* 움직이는 텍스트 */}
+        </p>
+        <p>
+          <marquee width="100" height="50">
+            여행지마다 명소 찾아가기
+          </marquee>
+          {/* 크기 조절 */}
+        </p>
+        <p>
+          <marquee behavior="scroll">나만의 실록 만들기</marquee>
+          {/* 스크롤의 속성 */}
+        </p>
+        <p>
+          <marquee loop="5">좋아하는 것과 관련된 작품 그리기</marquee>
+          {/* 스크롤의 속성 */}
+        </p>
+        <p>
+          <marquee scrolldelay="200">
+            제빵을 해서 주변인들과 나누기
+          </marquee>
+          {/* 스크롤의 속도 */}
+        </p>
+        <p>
+          <marquee hspace="50" vspace="50">
+            도자기 체험해보기
+          </marquee>
+          {/* 스크롤의 여백 */}
+        </p>
+        <Button onClick={handleStartClick}>시작하기</Button>
+      <Link to="/" style={{marginTop:"15px"}}>돌아가기</Link>
+      </MakeDreamListComponent>
+    );
+  }
 
 export default MakeDreamList;
